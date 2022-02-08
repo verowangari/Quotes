f9a49377d1784f5956a851069e9f7101989a93a0
verowangari <verowangari34@gmail.com>
2022-02-08 04:53:57 +0300

    add downvote and upvote buttons

--- src/app/componets/quote-details/quote-details.component.ts
+++ src/app/componets/quote-details/quote-details.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-
+  @Input() quoty: Quote;
+  @Output() isRead = new EventEmitter<boolean>();
+  deleteQuote(read:boolean){
+    this.isRead.emit(read);
+  }
+  upvote(){
+    this.quoty.likes+=1;
+  }
+  downvote(){
+    this.quoty.dislikes+=1;
+  }
   constructor() { }
 
   ngOnInit(): void {
