6948ce8d6fbb21b24bf18b88a5204ddace6d0936
verowangari <verowangari34@gmail.com>
2022-02-09 13:47:33 +0300

    add app-quotes in the root file for display

--- src/app/quote.spec.ts
+++ src/app/quote.spec.ts
@@ -2,6 +2,6 @@ import { Quote } from './quote';
 
 describe('Quote', () => {
   it('should create an instance', () => {
-    expect(new Quote()).toBeTruthy();
+    expect(new Quote(0,"","","","",new Date(),0,0)).toBeTruthy();
   });
 });
