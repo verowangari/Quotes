6d75bc4f9299e6183eab9f02cc3732ab92d4bfbf
verowangari <verowangari34@gmail.com>
2022-02-09 14:02:09 +0300

    add function that collects user input

--- src/app/components/quotes-form/quotes-form.component.ts
+++ src/app/components/quotes-form/quotes-form.component.ts
@@ -8,11 +8,11 @@ import { Quote } from 'src/app/quote';
 })
 export class QuotesFormComponent implements OnInit {
   quoted = new Quote(0, "", "", "", "", new Date(), 0, 0);
-  @Output() add = new EventEmitter<Quote>();
+  @Output() newQuote = new EventEmitter<Quote>();
 
   addQuote() {
   
-    this.add.emit(this.quoted);
+    this.newQuote.emit(this.quoted);
     this.quoted = new Quote(0, "", "", "", "", new Date(), 0, 0);
   }
   constructor() { }
